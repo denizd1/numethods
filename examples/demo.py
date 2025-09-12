3160be0076865952b18c90887b64a835c70a1c28
Deniz Dönmez <denzdonmez@gmail.com>
2025-09-12 09:33:36 +0300

    Add files via upload

--- examples/demo.py
+++ examples/demo.py
@@ -1,25 +1,47 @@
 from numethods import *
 
-A = Matrix([[2, -1], [1, 2], [1, 1]])
-b = Vector([1, 2, 3])
 
-# Factorization
-qr = QRHouseholder(A)
-Q, R = qr.Q, qr.R
-print("Q =", Q)
-print("R =", R)
+def demo_qr():
+    A = Matrix([[2, -1], [1, 2], [1, 1]])
 
-qrm = QRModifiedGramSchmidt(A)
-Qm, Rm = qrm.Q, qrm.R
-print("Qm =", Qm)
-print("Rm =", Rm)
-print("Q^T Q =", Q.T @ Q)
-print("Qm^T Qm =", Qm.T @ Qm)
-print("A=Qm Rm =", Qm @ Rm)
+    b = Vector([1, 2, 3])
 
-# Solve Ax = b (least squares, since A is tall)
-x_ls = LeastSquaresSolver(A, b).solve()
-print("Least squares solution:", x_ls)
+    # Factorization
+    qr = QRHouseholder(A)
+    Q, R = qr.Q, qr.R
+    print("Q =", Q)
+    print("R =", R)
+
+    qrm = QRModifiedGramSchmidt(A)
+    Qm, Rm = qrm.Q, qrm.R
+    print("Qm =", Qm)
+    print("Rm =", Rm)
+    print("Q^T Q =", Q.T @ Q)
+    print("Qm^T Qm =", Qm.T @ Qm)
+    print("A=Qm Rm =", Qm @ Rm)
+
+    # Solve Ax = b (least squares, since A is tall)
+    x_ls = LeastSquaresSolver(A, b).solve()
+    print("Least squares solution:", x_ls)
+
+
+def demo_eigen():
+    A = Matrix([[4, 1, 1], [1, 3, 0], [1, 0, 2]])
+    lam, v = PowerIteration(A, tol=1e-12).solve()
+    print("Power iteration:", lam, v)
+
+    lam_min, v_min = InversePowerIteration(A, shift=0.0, tol=1e-12).solve()
+    print("Inverse power:", lam_min, v_min)
+
+    lam_rqi, v_rqi = RayleighQuotientIteration(A, tol=1e-12).solve()
+    print("RQI:", lam_rqi, v_rqi)
+
+    Aq = QREigenvalues(A, tol=1e-12).solve()
+    print("QR diag eigs:", [Aq.data[i][i] for i in range(3)])
+
+    M = Matrix([[3, 1, 1], [-1, 3, 1], [1, 1, 3], [0, 2, 1]])
+    U, S, V = SVD(M).solve()
+    print("Singular values:", S)
 
 
 def demo_linear_solvers():
@@ -55,6 +77,8 @@ def demo_interpolation():
 
 
 if __name__ == "__main__":
+    demo_qr()
+    demo_eigen()
     demo_linear_solvers()
     demo_roots()
     demo_interpolation()
